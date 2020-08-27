832e6aa44f0f165fea01865719401b6f29669f07
mpustejovsky <mpustejovsky@yahoo.com>
2020-08-26 20:32:55 -0500

    fixed id

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const discoverMovie = () => {
     .then(function (response) {
       return response
     })
+
+
 }
 
 const getMovieById = (id) => {
@@ -25,7 +27,7 @@ const getMovieById = (id) => {
 }
 
 const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
+  const fakeId = 5783 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
   return axios.get(url)
